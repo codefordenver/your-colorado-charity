c0a8e7f8ebb52a8c1bd43437027841a316e8be46
Wm Lennon <milehighhokie@yahoo.com>
2020-02-02 18:58:46 -0700

    rolled back TableTop for CH service

--- src/index.js
+++ src/index.js
@@ -2,18 +2,31 @@ import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import CharityLayout from './CharityLayout';
 //import * as serviceWorker from './serviceWorker';
-import { HashRouter } from 'react-router-dom'
+import { Route, BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
 
 ReactDOM.render(
-    <HashRouter>
+    <BrowserRouter>
         <ScrollToTop>
-            <App></App>
+            <Route path="/" render={props => { const subdomain = window.location.hostname.split('.'); 
+            if (subdomain && subdomain.length > 1) 
+                return <CharityLayout {...props} subdomain={subdomain[0]}/>; 
+            return <App {...props}/>; }}
+            />
         </ScrollToTop>
-    </HashRouter>,
+    </BrowserRouter>,
     document.getElementById('root')
 );
+
+    // old code
+    // import { HashRouter } from 'react-router-dom'
+    // <HashRouter>
+    //     <ScrollToTop>
+    //         <App></App>
+    //     </ScrollToTop>
+    // </HashRouter>,
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
